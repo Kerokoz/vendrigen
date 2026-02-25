e3fc94dd61636181efa9ad066413be8a5dc610d2
Kerokoz <kerokogaming@gmail.com>
2026-02-25 22:24:26 +0300

    Connecting to messages

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from messages import team_message
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
     return "Welcome to Vendrigen 🚀"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
