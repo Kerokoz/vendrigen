9add353c627fd4f8dce3168caf73a76a18dd51db
Kerokoz <kerokogaming@gmail.com>
2026-02-25 22:52:24 +0300

    Chnages Made

--- messages.py
+++ messages.py
@@ -3,8 +3,3 @@ def team_message(team):
         return "Trust the process! Big season ahead 🔴"
     else:
         return "Every season is a new opportunity ⚽"
-
-
-if __name__ == "__main__":
-    team = input("Enter your team: ")
-    print(team_message(team))
